dd9b36bedcd1993cf6bae6e020730673f3f444b2
Denis <kuuushnirrr@gmail.com>
2023-02-17 18:12:00 +0200

    add DAL project

---
+++ LinkShortener.DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using LinkShortener.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkShortener.DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Link> Links { get; set; }
+    public DbSet<Click> Clicks { get; set; }
+
+    public AppDbContext()
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("PGSQL_STRING") + "LinkShortenerDB";
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+    
+}
